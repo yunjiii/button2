00aa6fc8c97dd3a8ee2b8fbfa95c7c03657e36e9
mju-oss-class <mju-oss-class@users.noreply.github.com>
2020-10-27 00:41:30 +0900

    Initial commit

---
+++ button1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window =Tk()
+
+b1 = Button(window, text="Yeonseung Ryu")
+b2 = Button(window, text="60161111")
+b1.grid(row=0, column=0)
+b2.grid(row=1, column=1)
+
+window.mainloop()
